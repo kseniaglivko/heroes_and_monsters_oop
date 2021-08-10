f9b6cb0f6517694226cae65a87b642f3e1643b5f
kseniaglivko <k.glivko@mail.ru>
2021-08-10 18:08:56 +0300

    Реализованы классы для генерации чудовищ.

--- monsters_factory.py
+++ monsters_factory.py
@@ -1,33 +1,26 @@
 """
-Фабрика по производству героев.
+Фабрика по производству чудовищ.
 """
 
 from abc import ABC, abstractmethod
 from time import sleep
 import random
-from game_stats import GameStats
-from game_class import Game
-from heroes_factory import Hero
+from game_stats import get_game_stats, update_game_stats
 
 
 class Monster(ABC):
     """Абстрактный класс чудовищ."""
 
-    def __init__(self, game):
+    def __init__(self):
         super().__init__()
-        self.game = game
-        self.monster_hp = hp
-        self.monster_power = power
-        self.game_stats = GameStats(self.game)
 
-    @abstractmethod
-    def spawn(self):
+    @staticmethod
+    def spawn():
         monster_spawner = {
             "evil_wizard": WizardFactory,
             "skeleton_archer": SkeletonFactory,
             "swordsman_goblin": GoblinFactory,
         }
-
         monsters_type_list = ["evil_wizard", "skeleton_archer", "swordsman_goblin"]
         spawner_type = random.choice(monsters_type_list)
         spawner = monster_spawner[spawner_type]()
@@ -35,57 +28,57 @@ class Monster(ABC):
         monster.spawn()
 
     @abstractmethod
-    def attack(self, power):
+    def attack(self):
         # В дальнейшем планируется выводить информацию о оставшемся здоровье у противника.
-        return f"Чудовище атаковало вас! Урон вашему здоровью составляет {power}."
+        monster_power = get_game_stats("monster", "power")
+        return f"Противник атакует! Урон вашему здоровью составляет {monster_power} единиц."
 
-    @abstractmethod
     def run(self):
-        return "Чудовище убежало от вас! Отправляемся дальше."
+        print("Противник бежал!")
+        sleep(1)
 
 
 class EvilWizard(Monster):
     """Класс злого чародея."""
 
-    def attack(self, power):
-        return f"Злой чародей атакует! Сила атаки - {power}."
-
-    def run(self):
-        return "Злой чародей убегает от вас!"
+    def attack(self):
+        monster_power = get_game_stats("monster", "power")
+        print(f"Злой чародей атакует! Сила атаки - {monster_power}.")
 
 
 class ArcherSkeleton(Monster):
     """Класс скелета-лучника."""
 
-    def attack(self, power):
-        return f"Скелет стреляет в вас из лука! Сила атаки - {power}."
-
-    def run(self):
-        return "Скелет убегает от вас!"
+    def attack(self):
+        monster_power = get_game_stats("monster", "power")
+        return f"Скелет стреляет в вас из лука! Сила атаки - {monster_power}."
 
 
 class SwordsmanGoblin(Monster):
     """Класс гоблина с мечом."""
 
-    def attack(self, power):
-        return f"Гоблин несется на вас с мечом! Сила атаки - {power}."
-
-    def run(self):
-        return "Гоблин убегает от вас!"
+    def attack(self):
+        monster_power = get_game_stats("monster", "power")
+        return f"Гоблин несется на вас с мечом! Сила атаки - {monster_power}."
 
 
 class MonsterFactory(ABC):
     """Абстрактная фабрика игрового противника."""
 
     @abstractmethod
-    def create_enemy(self):
+    def create_monster(self):
         pass
 
 
 class WizardFactory(MonsterFactory):
     """Фабрика по производству злых чародеев."""
 
-    def create_monter(self):
+    def create_monster(self):
+        power = random.randint(6, 25)
+        hp = random.randint(10, 40)
+        update_game_stats("monster", power, "power")
+        update_game_stats("monster", "Злой колдун", "type")
+        update_game_stats("monster", hp, "hp")
         return EvilWizard()
 
 
@@ -93,6 +86,11 @@ class SkeletonFactory(MonsterFactory):
     """Фабрика по производству скелетов-лучников."""
 
     def create_monster(self):
+        power = random.randint(2, 15)
+        hp = random.randint(5, 25)
+        update_game_stats("monster", power, "power")
+        update_game_stats("monster", "Скелет-лучник", "type")
+        update_game_stats("monster", hp, "hp")
         return ArcherSkeleton()
 
 
@@ -100,4 +98,9 @@ class GoblinFactory(MonsterFactory):
     """Фабрика по производству гоблинов с мечом."""
 
     def create_monster(self):
+        power = random.randint(4, 20)
+        hp = random.randint(5, 35)
+        update_game_stats("monster", power, "power")
+        update_game_stats("monster", "Гоблин-мечник", "type")
+        update_game_stats("monster", hp, "hp")
         return SwordsmanGoblin()
