8174f9baa2f0f666c4f51ef24ebbecffbc876821
kseniaglivko <k.glivko@mail.ru>
2021-08-10 18:49:55 +0300

    Доработаны классы игры и игровой статистики.

--- game_class.py
+++ game_class.py
@@ -4,7 +4,7 @@
 """
 
 import random
-from game_stats import get_game_stats, reset_stats, choose_hero
+from game_stats import GameStats
 from heroes_factory import Hero
 from monsters_factory import Monster
 from items_factory import Item
@@ -12,24 +12,25 @@ from items_factory import Item
 
 class Game:
     def __init__(self):
+        self.game_stats = GameStats(self)
         self.hero = Hero(self)
-        self.monster = Monster(self)
+        self.monster = Monster()
         self.item = Item(self)
 
     def game_launch(self):
-        reset_stats()
         print(
             "Добро пожаловать в игру 'Герой и Чудовища 2: Волшебный тотем'!"
             "\nВам предстоит спасти королевство от нападения 10 чудовищ."
             "\nКоличество ваших жизней - 15, а сила вашей атаки - 10."
             "\nУдачи!"
         )
-        choose_hero()
+        self.game_stats.choose_hero(self)
         self.run_game()
 
     def run_game(self):
-        if get_game_stats("monster_counter") >= 10:
+        if self.game_stats.get_game_stats("monster_counter") >= 10:
             print("Вы спасли королевство от чудовищ и победили! Поздравляем!")
+            self.game_stats.reset_stats()
             exit()
         else:
             random_action_selector = [self.monster.spawn, self.item.spawn]
--- game_stats.py
+++ game_stats.py
@@ -2,7 +2,6 @@
 Модуль, собирающий статистику о процессе игры. С помощью него будет реализовано сохранение игры.
 """
 
-import os
 import json
 import jsonschema
 from statistics_schema import statistics_schema
@@ -11,53 +10,71 @@ from statistics_schema import statistics_schema
 # Дефолтные параметры игровой статистики. С ними начинаем игру.
 default_parameters = {
     "monster_counter": 0,
+    "apple": 0,
+    "totem": 0,
     "hero": {"type": "", "power": 10, "hp": 15},
     "monster": {"type": "", "power": "", "hp": ""},
     "inventory": "[{'Меч': 10}]",
 }
 
 
-def get_game_stats(from_, what=None):
-    """Функция, выводящая игровую статистику."""
-    with open("game_process_info.json", "r") as file:
+class GameStats:
+    def __init__(self, game):
+        self.game = game
+
+    @staticmethod
+    def reset_stats():
+        """Функция, обнуляющая игровую статистику."""
+        with open("game_process_info.json", "w") as file:
+            json.dump(default_parameters, file)
+
+    @staticmethod
+    def get_game_stats(from_, what=None):
+        """Функция, выводящая игровую статистику."""
+        with open("game_process_info.json", "r") as file:
+            try:
+                game_info = json.load(file)
+                return game_info[from_][what]
+            except json.decoder.JSONDecodeError:
+                raise Exception(
+                    "Ошибка инициализации игровой статистики. Пожалуйста, начните игру заново."
+                )
+
+    @staticmethod
+    def update_game_stats(from_, value, what=None):
+        """Функция, обновляющая игровую статистику."""
+        file = open("game_process_info.json", "w+")
         try:
             game_info = json.load(file)
-            return game_info[from_][what]
+            game_info[from_][what] = value
+            return json.dump(game_info, file)
         except json.decoder.JSONDecodeError:
             raise Exception(
                 "Ошибка инициализации игровой статистики. Пожалуйста, начните игру заново."
             )
 
+    @staticmethod
+    def save_game():
+        """Функция,сохраняющая игру."""
+        with open("game_process_info.json", "r") as from_:
+            with open("saved_game.json", "w+") as to:
+                to.write(from_.read())
 
-def update_game_stats(from_, value, what=None):
-    """Функция, обновляющая игровую статистику."""
-    file = open("game_process_info.json", "w+")
-    try:
-        game_info = json.load(file)
-        game_info[from_][what] = value
-        return json.dump(game_info, file)
-    except json.decoder.JSONDecodeError:
-        raise Exception(
-            "Ошибка инициализации игровой статистики. Пожалуйста, начните игру заново."
-        )
+    def load_game(self):
+        """Функция, загружающая сохраненную игру."""
+        with open("saved_game.json", "r") as from_:
+            with open("game_process_info.json", "w+") as to:
+                to.write(from_.read())
+                self.game.run_game()
 
-
-def reset_stats():
-    """Функция, обнуляющая игровую статистику."""
-    try:
-        os.remove("game_process_info.json")
-    except FileNotFoundError:
-        pass
-    json.dumps(default_parameters)
-
-
-def choose_hero():
-    hero_input = input(
-        "Пожалуйста, выберите класс для вашего героя - Маг, Мечник или Лучник): "
-    ).capitalize()
-    try:
-        jsonschema.validate({"hero[type]": "hero_input"}, statistics_schema)
-    except jsonschema.exceptions.ValidationError:
-        print("Пожалуйста, выберите один из предложенные классов.")
-        choose_hero()
-    update_game_stats("hero", hero_input, "type")
+    @staticmethod
+    def choose_hero(self):
+        hero_input = input(
+            "Пожалуйста, выберите класс для вашего героя - Маг, Мечник или Лучник): "
+        ).capitalize()
+        try:
+            jsonschema.validate({"hero[type]": "hero_input"}, statistics_schema)
+        except jsonschema.exceptions.ValidationError:
+            print("Пожалуйста, выберите один из предложенные классов.")
+            self.choose_hero()
+        self.update_game_stats("hero", hero_input, "type")
